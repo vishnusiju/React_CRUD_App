801a950ff65615468311b0269081afd682467274
vishnusiju <vishnusiju123@gmail.com>
2021-06-26 11:25:20 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Todo from './todo';
+import './App.css';
+
+function App() {
+ 
+  return (
+    
+    <div>
+        
+        <div>
+             <Todo/>
+        </div>
+    </div>
+    
+  );
+}
+
+export default App;
